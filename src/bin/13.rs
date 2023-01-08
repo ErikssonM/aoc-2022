19ba901686f1e06cc93ea240b2f1c1dc57ccc39b
Martin Eriksson <60point3@gmail.com>
2023-01-08 20:53:21 +0100

    Day 13 task 2 solved. Changed from PartialOrd to Ord (but that was not used by sort????). I think I'll have to spend some time on PartialOrd/Ord

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -7,12 +7,12 @@ use nom::{
     bytes::complete::tag,
     character::complete::one_of,
     combinator::{all_consuming, map, map_res, recognize},
-    multi::{many0, separated_list0},
+    multi::{many0, many1, separated_list0},
     sequence::{preceded, separated_pair, terminated},
     IResult,
 };
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, PartialOrd)]
 enum Packet {
     Int(u32),
     List(Vec<Packet>),
@@ -50,45 +50,51 @@ fn parse_pair(input: &str) -> IResult<&str, Pair> {
     )(input)
 }
 
-fn parse(input: &str) -> Vec<Pair> {
+fn parse_all_pairs(input: &str) -> Vec<Pair> {
     all_consuming(separated_list0(tag("\n\n"), parse_pair))(input)
         .unwrap()
         .1
 }
 
-impl PartialOrd for Packet {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(match (self, other) {
+fn parse_all_lines(input: &str) -> Vec<Packet> {
+    all_consuming(separated_list0(many1(tag("\n")), parse_packet_list))(input)
+        .unwrap()
+        .1
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
             (Packet::Int(s), Packet::Int(o)) => s.cmp(o),
             (Packet::List(s), Packet::List(o)) => {
                 let mut zipped = zip(s, o);
                 let _ = zipped
-                    .take_while_ref(|&(si, oi)| si.partial_cmp(oi).unwrap() == Ordering::Equal)
+                    .take_while_ref(|&(si, oi)| si.cmp(oi) == Ordering::Equal)
                     .collect::<Vec<(&Packet, &Packet)>>();
                 if let Some((si, oi)) = zipped.next() {
-                    si.partial_cmp(&oi).unwrap()
+                    si.cmp(&oi)
                 } else {
                     s.len().cmp(&o.len())
                 }
             }
-            (left_list, Packet::Int(right)) => left_list
-                .partial_cmp(&Packet::List(vec![Packet::Int(*right)]))
-                .unwrap(),
-            (Packet::Int(left), right_list) => Packet::List(vec![Packet::Int(*left)])
-                .partial_cmp(right_list)
-                .unwrap(),
-        })
+            (left_list, Packet::Int(right)) => {
+                left_list.cmp(&Packet::List(vec![Packet::Int(*right)]))
+            }
+            (Packet::Int(left), right_list) => {
+                Packet::List(vec![Packet::Int(*left)]).cmp(right_list)
+            }
+        }
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let pairs = parse(input.trim());
+    let pairs = parse_all_pairs(input.trim());
 
     let mut sum = 0;
     for (index, pair) in pairs.iter().enumerate() {
         let Pair { left, right } = pair;
 
-        if left < right {
+        if left.cmp(&right) == Ordering::Less {
             sum += index + 1;
         }
     }
@@ -97,7 +103,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut packet_lines = parse_all_lines(input.trim());
+
+    let divider_1 = Packet::List(vec![Packet::List(vec![Packet::Int(2)])]);
+    let divider_2 = Packet::List(vec![Packet::List(vec![Packet::Int(6)])]);
+
+    packet_lines.push(divider_1.clone());
+    packet_lines.push(divider_2.clone());
+
+    packet_lines.sort_by(Packet::cmp);
+
+    let index_1 = packet_lines
+        .iter()
+        .find_position(|&p| p.cmp(&divider_1) == Ordering::Equal)
+        .unwrap()
+        .0
+        + 1;
+
+    let index_2 = packet_lines
+        .iter()
+        .find_position(|&p| p.cmp(&divider_2) == Ordering::Equal)
+        .unwrap()
+        .0
+        + 1;
+
+    Some((index_1 * index_2).try_into().unwrap())
 }
 
 fn main() {
@@ -119,7 +149,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 13);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(140));
     }
 
     #[test]
@@ -169,7 +199,7 @@ mod tests {
                 right: Packet::List(vec![Packet::Int(3)]),
             },
         ];
-        assert_eq!(parse(input), expected);
+        assert_eq!(parse_all_pairs(input), expected);
     }
 
     #[test]
